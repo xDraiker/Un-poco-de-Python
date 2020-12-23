b347a0910f345d2f1d27f885ec2b13dcfd17b0ef
Draiker <daniel.vz.gc97@gmail.com>
2020-12-22 20:31:18 -0600

    Dia 1

---
+++ 1_Sinaxis/Sintaxis.py
@@ -0,0 +1,13 @@
+#declaracion de una variable
+x=5.9
+z=9
+y='Hola'
+Y=' que hace'
+#prin("")->imprime en consolalo que esa ()
+print(y+Y)#concaenacion de sring(str)
+print(x+z)#suma de dos numeros
+#type(variable)->devuelve el ipo de variable ()
+print(type(x))
+print(type(y))
+
+
---
+++ 2_ConvercionDeVariables/convercion.py
@@ -0,0 +1,11 @@
+#convercion de un numero a una cadena
+x=5
+Cx= str(x)
+#convercion de una cadena a un numero enero
+y='8'
+Ny= int(y)
+
+r=str(x+Ny)
+
+print("La suma de " + Cx + " + " + y + " = " + r)
+#remarcar que solo se oueden concatenar cadenas no numeros
